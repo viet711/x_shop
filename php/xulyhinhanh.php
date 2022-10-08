b492cbc730a1fb34d6c5d127ed3f0bd61d1542d2
viet <viet20000333@gmail.com>
2022-10-08 10:29:13 +0700

    20221008

--- php/xulyhinhanh.php
+++ php/xulyhinhanh.php
@@ -1,13 +1,10 @@
 <?php
     if(!isset($_POST['request']) && !isset($_GET['request'])) die(null);
 
-    // Hướng dẫn tại https://stackoverflow.com/questions/17122218/get-all-the-images-from-a-folder-in-php 
-
+\
     switch ($_POST['request']) {
-    	// lấy tất cả hình ảnh banners
     	case 'getallbanners':
 				$directory = "../img/banners";
-                // lấy file ảnh định dạng png hoặc gif
                 $images = glob($directory . "/*.{png,gif}", GLOB_BRACE); 
                 die (json_encode($images));
     		break;
@@ -19,7 +16,6 @@
             break;
     	
     	default:
-    		# code...
     		break;
     }
 ?>
